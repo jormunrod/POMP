14c82d0e99ed6d5bd7506332915c105f24eeba42
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2026-02-15 12:45:49 +0100

    feat: Add enemy mechanics and spawn logic to the game

--- constants.lua
+++ constants.lua
@@ -1,5 +1,9 @@
 return {
     PLAYER_SIZE = 32,
     PLAYER_SPEED = 200, -- px/s
-    PLAYER_BOTTOM_MARGIN = 10
+    PLAYER_BOTTOM_MARGIN = 10,
+    ENEMY_SIZE = 32,
+    ENEMY_SPEED = 100,      -- px/s
+    ENEMY_BOTTOM_MARGIN = 10,
+    ENEMY_SPAWN_RATE = 0.5, -- seconds
 }
--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 local K = require("constants")
 
 local player
+local enemies
+local spawn_timer
 
 function love.load()
     local screen_width = love.graphics.getWidth()
     local screen_height = love.graphics.getHeight()
 
-
+    -- Create player
     player = {
         x = 0,
         y = 0,
@@ -17,20 +19,55 @@ function love.load()
 
     player.x = screen_width / 2 - player.w / 2
     player.y = screen_height - player.h - K.PLAYER_BOTTOM_MARGIN
+
+    -- Enemies
+    enemies = {}
+    spawn_timer = 0
 end
 
 function love.draw() -- Executed in loop to draw the screen
     love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
+    for i, enemy in ipairs(enemies) do
+        love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.w, enemy.h)
+    end
 end
 
 function love.update(dt) -- Executed on every frame to calculate the logic
     -- love.keyboard.isDown(key) returns true while key is pressed.
     -- dt (delta time): time since the last frame
 
+    -- Player movement
     if love.keyboard.isDown("right") then
         player.x = player.x + player.speed * dt
     end
     if love.keyboard.isDown("left") then
         player.x = player.x - player.speed * dt
+    end
+
+    -- Enemies spawn
+    spawn_timer = spawn_timer - dt
+    if spawn_timer <= 0 then
+        spawn_timer = K.ENEMY_SPAWN_RATE
+
+        local new_enemy = {
+            x = love.math.random(0, love.graphics.getWidth() - K.ENEMY_SIZE),
+            y = -K.ENEMY_SIZE,
+            w = K.ENEMY_SIZE,
+            h = K.ENEMY_SIZE,
+            speed = K.ENEMY_SPEED
+        }
+
+        table.insert(enemies, new_enemy)
+    end
+
+    -- Enemies movement and cleanup
+    for i = #enemies, 1, -1 do
+        local enemy = enemies[i]
+
+        enemy.y = enemy.y + enemy.speed * dt
+
+        if enemy.y > love.graphics.getHeight() then
+            table.remove(enemies, i)
+        end
     end
 end
